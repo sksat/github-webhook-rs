28a71cee79f094e77265324eddf7b0b06693f1e7
ylides <e.propagating@gmail.com>
2023-04-17 02:55:21 +0900

    add comment

--- type-generator/src/transformer/internal_tag.rs
+++ type-generator/src/transformer/internal_tag.rs
@@ -2,24 +2,28 @@ use std::collections::HashMap;
 
 use crate::ir::*;
 
+/// find tag from rust enum and attr to it.
 pub fn adapt_internal_tag(segment: &mut RustSegment, lkm: &LiteralKeyMap) -> Option<()> {
     if let RustSegment::Enum(re) = segment {
-        let mut props: HashMap<String, String> = Default::default();
+        let mut cand_props: HashMap<String, String> = Default::default();
         for memb in &re.member {
-            let inter = &memb.as_unary()?.name;
-            let map = lkm.get(inter)?;
-            if props.is_empty() {
-                props = map.clone();
+            let tname = &memb.as_unary()?.name;
+            let props = lkm.get(tname)?;
+            if cand_props.is_empty() {
+                cand_props = props.clone();
                 continue;
             }
-            props.retain(|k, _| map.contains_key(k));
-            if props.is_empty() {
+            // calc intersection of all enum members
+            cand_props.retain(|k, _| props.contains_key(k));
+            if cand_props.is_empty() {
                 return None;
             }
         }
-        // assert !props.is_empty()
-        assert_eq!(props.len(), 1);
-        let tag_name = props.keys().next().unwrap().to_owned();
+        assert!(!cand_props.is_empty());
+        if cand_props.len() != 1 {
+            return None;
+        }
+        let tag_name = cand_props.keys().next().unwrap().to_owned();
         for memb in &mut re.member {
             let inter = &memb.as_unary().unwrap().name;
             let variant_name = lkm.get(inter).unwrap().get(&tag_name).unwrap().to_owned();
