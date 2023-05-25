9ececb80056c6c96102bbf8d2faec7a526528179
sksat <sksat@sksat.net>
2023-05-26 05:46:59 +0900

    github-webhook: get metadata in build script

--- github-webhook/build.rs
+++ github-webhook/build.rs
@@ -1,8 +1,29 @@
 use anyhow::Result;
+use cargo_metadata::{CargoOpt, MetadataCommand};
+use std::env;
 
 use github_webhook_dts_downloader::run_transform;
 
 fn main() -> Result<()> {
+    let manifest_dir = env!("CARGO_MANIFEST_DIR").to_string();
+    let metadata = MetadataCommand::new()
+        .manifest_path(manifest_dir + "/Cargo.toml")
+        .features(CargoOpt::AllFeatures)
+        .exec()
+        .unwrap();
+
+    // workspace manifest
+    assert_ne!(metadata.workspace_members.len(), 0);
+
+    let pkg_name = env!("CARGO_PKG_NAME");
+    let pkg = &metadata
+        .packages
+        .iter()
+        .find(|p| p.name == pkg_name)
+        .unwrap();
+
+    assert_eq!(pkg_name, pkg.name);
+
     run_transform(Default::default())?;
 
     Ok(())
